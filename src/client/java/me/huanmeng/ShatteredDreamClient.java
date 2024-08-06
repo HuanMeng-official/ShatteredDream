262c33215848902f9926e7bda1c42254dad035af
HuanMeng-official <3099044113@qq.com>
2024-08-06 22:15:50 +0800

    init commit

---
+++ src/client/java/me/huanmeng/ShatteredDreamClient.java
@@ -0,0 +1,9 @@
+package me.huanmeng;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ShatteredDreamClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+	}
+}
