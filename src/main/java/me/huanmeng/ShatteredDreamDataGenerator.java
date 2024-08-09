dd7558d072137688f0da8656006a03229df26687
HuanMeng-official <3099044113@qq.com>
2024-08-09 23:24:58 +0800

    add: added datagen

--- src/main/java/me/huanmeng/ShatteredDreamDataGenerator.java
+++ src/main/java/me/huanmeng/ShatteredDreamDataGenerator.java
@@ -1,11 +1,13 @@
 package me.huanmeng;
 
+import me.huanmeng.datagen.ModelsProvider;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
 public class ShatteredDreamDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
-
+		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
+		pack.addProvider(ModelsProvider::new);
 	}
 }
---
+++ src/main/java/me/huanmeng/datagen/ModelsProvider.java
@@ -0,0 +1,23 @@
+package me.huanmeng.datagen;
+
+import me.huanmeng.item.SDitems;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
+import net.minecraft.data.client.BlockStateModelGenerator;
+import net.minecraft.data.client.ItemModelGenerator;
+import net.minecraft.data.client.Models;
+
+public class ModelsProvider extends FabricModelProvider {
+    public ModelsProvider(FabricDataOutput output) {
+        super(output);
+    }
+
+    @Override
+    public void generateBlockStateModels(BlockStateModelGenerator blockStateModelGenerator) {
+    }
+
+    @Override
+    public void generateItemModels(ItemModelGenerator itemModelGenerator) {
+        itemModelGenerator.register(SDitems.DREAM_SPLINTER, Models.GENERATED);
+    }
+}
